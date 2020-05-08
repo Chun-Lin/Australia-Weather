0c63a519da29ee5d63ae76f2d528fc832cfd3499
Chun-Lin <wulin40063@gmail.com>
2020-05-08 14:26:02 +0800

    feat: 🎸 add pan and zoom features to AU Country map

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,10 +1,28 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useRef, useEffect, useState } from 'react'
 import * as d3 from 'd3'
 
 import auGeoJson from './geoData/australia.geo.json'
 import auCityGeoJson from './geoData/australia.cities.geo.json'
 
 const Country = ({ width, height, population = 100000 }) => {
+  const [{ x, y, k }, setTransform] = useState({ x: 0, y: 0, k: 1 })
+  const svgRef = useRef(null)
+
+  const zoom = useMemo(
+    () =>
+      d3
+        .zoom()
+        .scaleExtent([1, 5])
+        .on('zoom', () => setTransform(d3.event.transform)),
+    []
+  )
+
+  useEffect(() => {
+    if (!svgRef.current) return
+
+    d3.select(svgRef.current).call(zoom)
+  }, [zoom])
+
   const projection = d3
     .geoMercator()
     .center([134, -26]) // GPS of location to zoom on
@@ -22,8 +40,13 @@ const Country = ({ width, height, population = 100000 }) => {
   )
 
   return (
-    <svg width={width} height={height} style={{ background: 'lightgrey' }}>
-      <g>
+    <svg
+      ref={svgRef}
+      width={width}
+      height={height}
+      style={{ background: 'lightgrey' }}
+    >
+      <g transform={`translate(${x}, ${y}) scale(${k})`}>
         <path d={geoPath(auGeoJson)} />
         <g text-anchor="middle" font-family="sans-serif" font-size="10">
           {highPopulationAuCityFeatures.map(
